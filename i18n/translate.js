0a725578d7a3b59bed471207027585f1954fc5e6
popawan <88959338+popawan@users.noreply.github.com>
2025-08-20 15:27:23 +0800

    translate.js

--- i18n/translate.js
+++ i18n/translate.js
@@ -1,1 +1,16 @@
+import { createI18n } from 'vue-i18n'
+import zh from './locales/zh.json'
+import en from './locales/en.json'
+import jp from './locales/jp.json'
+import kr from './locales/kr.json'
+
+const messages = { zh, en, jp, kr }
+
+const i18n = createI18n({
+  locale: 'zh',
+  fallbackLocale: 'en',
+  messages,
+})
+
+export default i18n
 
